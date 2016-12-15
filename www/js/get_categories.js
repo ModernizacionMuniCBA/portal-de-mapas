9dcc5f21688f5d628fffef828ec19c3adf339c3e
Gaston Ramirez <gastonframirez@gmail.com>
2016-12-15 00:23:31 -0300

    Agrega lectura de JSON + Cambios de CSS

--- www/js/get_categories.js
+++ www/js/get_categories.js
@@ -1,14 +1,62 @@
 		var gobAbiertoAPI = "https://gobiernoabierto.cordoba.gob.ar/api";
 		var gobAbiertoAPI_categories = "/tipo-actividad/"
 		var formatJson = "?format=json";
+		var map;
+		var layers = [];
+		function initMap() {
+			map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 14,
+				center: {lat: -31.420180, lng: -64.188787}
+			});
+
+			layers[0] = new google.maps.KmlLayer({
+				url: '',
+				preserveViewport: true
+			});
+		}
 
 		$.ajax({
 			dataType: "json",
-			url: gobAbiertoAPI+gobAbiertoAPI_categories+formatJson,
+			url: "https://modernizacionmunicba.github.io/portal-de-mapas/www/generated.json",
 			success: handleData
 		});
 		function handleData(data) {
 			$.each(data.results, function(i, category) {
-				$('#categories-menu').append('<div class="row" style="margin-top:5px; margin-left:5px;"><li><a href="tipo.html#tipo-'+category.id+'"><div class="col-xs-3"><div class="circle-image-li" style="background-image: url(img/default-event.png);"></div></div><div class="col-xs-9">'+category.nombre+'</div></a></li></div>');
+					$('#accordion').append('<div class="panel panel-mapas"><div class="panel-heading collapsed" role="tab" id="heading-'+category.id+'" data-toggle="collapse" data-parent="#accordion" href="#collapse-'+category.id+'" aria-expanded="false" aria-controls="collapse-'+category.id+'"><h4 class="panel-title"><a class="collapsed" role="button">'+category.titulo+'</a></h4><h5 class="panel-title panel-subtitle"><a class="collapsed" role="button">'+category.descripcion+'</a></h5></div><div id="collapse-'+category.id+'" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-'+category.id+'"><div class="panel-body" id="category-'+category.id+'-menu">');
+					$.each(category.subcategorias, function(j, layer) {
+						$('#category-'+category.id+'-menu').append('<a href="#mapa-'+layer.id+'"><div class="map-layer-selector" id="mapa-'+layer.id+'" style="cursor: pointer;" onclick="if(!$(this).hasClass(\'active\')){toggleLayer('+layer.id+');} setActiveLayer(this);"><h4 class="title">'+layer.titulo+'</h4><h5 class="title subtitle">'+layer.descripcion+'</h5></div></a>');
+						layers[layer.id] = new google.maps.KmlLayer({
+						  url: layer.url,
+						  preserveViewport: true
+						});
+					});
+					$('#accordion').append('</div></div>');
 			});
+			var url = document.location.toString();
+			if (url.match('#')) {
+				var string = url.split('#')[1];
+				var type = string.split('-')[0];
+				var valor = string.split('-')[1];
+				if (type == "mapa"){
+					toggleLayer(valor);
+					$('#mapa-'+valor).addClass("active");
+					var panel = $('#mapa-'+valor).closest('.collapse');
+					panel.collapse();
+				}
+			}
+		}
+
+		function toggleLayer(i) {
+			for (var j = 0; j < layers.length; j++) {
+				if(layers[j].getMap() !== null) {
+					layers[j].setMap(null);
+				}
+			}
+			if(layers[i].getMap() === null) {
+				layers[i].setMap(map);
+			}
+		}
+		function setActiveLayer(obj) {
+			$('.map-layer-selector.active').removeClass('active');
+			$(obj).addClass("active");
 		}
